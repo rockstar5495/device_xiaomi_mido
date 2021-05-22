3346721d3d600e0b0680f278698a75ccde540167
Pratyaksh Bharadwaj <54440925+rockstar5495@users.noreply.github.com>
2021-05-22 17:40:42 +0530

    mido: updated HAL's paths
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 # remove hals
 rm -rf hardware/qcom-caf/msm8996/audio
-rm -rf hardware/qcom-caf/msm8996/deiplay
+rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/msm8996/media
 
 # CAF HALS
-git clone https://github.com/shashank1436/android_hardware_qcom_audio.git -b r11 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/shashank1436/android_hardware_qcom_display.git -b r11 hardware/qcom-caf/msm8996/display
-git clone https://github.com/shashank1436/android_hardware_qcom_media.git -b r11 hardware/qcom-caf/msm8996/media
+git clone https://github.com/shashank1436/android_hardware_qcom_audio.git -b r11 hardware/qcom-caf/msm8996-r/audio
+git clone https://github.com/shashank1436/android_hardware_qcom_display.git -b r11 hardware/qcom-caf/msm8996-r/display
+git clone https://github.com/shashank1436/android_hardware_qcom_media.git -b r11 hardware/qcom-caf/msm8996-r/media
